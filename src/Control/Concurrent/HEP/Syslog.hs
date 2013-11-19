b7bdb2ffd9d1f5b70c2a8ada7f43cc8d84adde79
ice <ice@fxhome.local>
2013-11-20 00:21:47 +0900

    init

---
+++ src/Control/Concurrent/HEP/Syslog.hs
@@ -0,0 +1,92 @@
+{-#LANGUAGE DeriveDataTypeable#-}
+{-#LANGUAGE BangPatterns#-}
+
+module Control.Concurrent.HEP.Syslog 
+    ( startSyslog
+    , startSyslogSupervisor
+    , syslogError
+    , syslogInfo
+    , stopSyslog
+    )
+    where
+
+import Control.Concurrent.HEP
+import Control.Concurrent
+import Data.Typeable
+import Control.Monad.Trans
+import Control.Monad
+import System.Posix.Syslog
+
+data LoggerMessage = LogError String
+                   | LogInfo String
+                   | LogStop
+                   | LogPing (MBox LogAnswer)
+    deriving (Typeable)
+
+data LogAnswer = LogAnswerOK
+
+instance Message LoggerMessage
+
+pid = "HEPSysLogger"
+
+startSyslogSupervisor:: String-> HEPProc-> HEP Pid
+startSyslogSupervisor ident sv = do
+    !pid <- spawn $! procWithSupervisor (proc sv) $! 
+        procWithBracket loggerInit loggerShutdown $! procRegister pid $!
+        proc $! loggerWorker ident
+    inbox <- liftIO newMBox
+    send pid $! LogPing inbox
+    ret <- liftIO $! receiveMBoxAfter 1000 inbox
+    case ret of 
+        Just _ -> return pid
+        _ -> do
+            liftIO $! ioError $! userError $! "unable to spawn syslog client"
+            return pid
+
+startSyslog:: String-> HEP Pid
+startSyslog ident = do
+    spawn $! procWithBracket loggerInit loggerShutdown $! 
+        procRegister pid $! proc $! loggerWorker ident
+
+syslogInfo s = do
+    send ( toPid pid) $! LogInfo s
+    
+syslogError s = send (toPid pid)  $! LogError s
+
+stopSyslog = send (toPid pid) LogStop
+
+loggerInit = do
+    p <- self
+    send p $! LogInfo "syslog client started"
+    procRunning
+
+loggerShutdown = do
+    procRunning
+
+loggerWorker:: String-> HEP HEPProcState
+loggerWorker ident = do
+    msg <- receive
+    case fromMessage msg of
+        Nothing-> procRunning
+        Just (LogPing outbox) -> do
+            liftIO $! sendMBox outbox LogAnswerOK
+            procRunning
+        Just LogStop -> do
+            spawn $! procForker forkOS $! proc $! 
+                _intSyslog ident Info "syslog client stopped"
+            procFinished
+        
+        Just (LogError string) -> do
+            spawn $! procForker forkOS $! proc $!
+                _intSyslog ident Error string
+            procRunning
+        Just (LogInfo string) -> do
+            spawn $! procForker forkOS $! 
+                proc $!  _intSyslog ident Info string
+            procRunning
+        
+_intSyslog:: String-> Priority-> String-> HEP HEPProcState
+_intSyslog ident prio s = do
+    liftIO $! useSyslog ident $! syslog prio s
+    procFinished
+    
